83b66932a6fd4729830614aa3a0f3d5ed56aa6af
K M Dilshan Udara <dilshankm@outlook.com>
2025-11-04 01:23:24 +0000

    fix: add govuk support body class

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { initAll } from 'govuk-frontend'
 import App from './App.tsx'
 import './styles/main.scss'
 
+if (typeof document !== 'undefined' && document.body) {
+  document.body.classList.add('govuk-frontend-supported', 'js-enabled')
+}
+
 initAll()
 
 createRoot(document.getElementById('root')!).render(
